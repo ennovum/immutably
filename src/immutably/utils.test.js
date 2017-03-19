dd7a8694eaa5d6be8efe5c14e429d4712efd902a
Grzegorz Wasiak <g.wasiak@gmail.com>
2017-03-19 19:13:49 +0100

    reduce tests

--- src/immutably/utils.test.js
+++ src/immutably/utils.test.js
@@ -2,7 +2,8 @@ import test from 'tape';
 
 import {
     clone, arrayClone, objectClone,
-    isPrimitive
+    isPrimitive,
+    reduce, objectReduce
 } from './utils';
 
 test('immutably / utils / clone with type detect', (testCase) => {
@@ -103,7 +104,7 @@ test('immutably / utils / clone an object', (testCase) => {
 });
 
 test('immutably / utils / checks primitive values', (testCase) => {
-    const testScenario = () => {
+    const testScenario1 = () => {
         testCase.equal(isPrimitive(null), true);
         testCase.equal(isPrimitive(undefined), true);
         testCase.equal(isPrimitive(1), true);
@@ -113,7 +114,57 @@ test('immutably / utils / checks primitive values', (testCase) => {
         testCase.equal(isPrimitive([]), false);
     };
 
-    testCase.doesNotThrow(testScenario);
+    testCase.doesNotThrow(testScenario1);
+
+    testCase.end();
+});
+
+test('immutably / utils / performs reduce with type check', (testCase) => {
+    const testScenario1 = () => {
+        const input = [1, 2, 3, 4];
+        const output = reduce(input, (output, value, index) => {
+            output[index] = value;
+            return output;
+        }, []);
+
+        testCase.deepEqual(output, input);
+    };
+
+    testCase.doesNotThrow(testScenario1);
+
+    const testScenario2 = () => {
+        const input = {a: 1, b: 2, c: 3, d: 4};
+        const output = reduce(input, (output, value, key) => {
+            output[key] = value;
+            return output;
+        }, {});
+
+        testCase.deepEqual(output, input);
+    };
+
+    testCase.doesNotThrow(testScenario2);
+
+    testCase.end();
+});
+
+test('immutably / utils / performs object reduce', (testCase) => {
+    const testScenario1 = () => {
+        const input = {a: 1, b: 2, c: 3, d: 4};
+        const output = objectReduce(input, (output, value) => output + value, 0);
+
+        testCase.equal(output, 10);
+    };
+
+    testCase.doesNotThrow(testScenario1);
+
+    const testScenario2 = () => {
+        const input = {a: 1, b: 2, c: 3, d: 4};
+        const output = objectReduce(input, (output, value, key) => output + key, '!');
+
+        testCase.equal(output, '!abcd');
+    };
+
+    testCase.doesNotThrow(testScenario2);
 
     testCase.end();
 });
