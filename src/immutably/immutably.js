9a8f84f5e9eace485ef3c174595aee5a44178e56
Grzegorz Wasiak <g.wasiak@gmail.com>
2017-03-19 19:31:14 +0100

    immutably.merge implementation

--- src/immutably/immutably.js
+++ src/immutably/immutably.js
@@ -1,7 +1,8 @@
 import apply from './apply';
 import set from './set';
+import merge from './merge';
 
-const immutably = {apply, set};
+const immutably = {apply, set, merge};
 
 export default immutably;
 export {immutably};
---
+++ src/immutably/merge.js
@@ -0,0 +1,36 @@
+import {apply} from './apply';
+import {clone, isPrimitive, reduce} from './utils';
+
+function merge(input, path, value, output) {
+    return apply(input, path, (input, output) => valueMerge(input, value, output), output);
+}
+
+function valueMerge(input, value, output) {
+    if (isPrimitive(value)) {
+        return value;
+    }
+    return recurrentMerge(input, value, output);
+}
+
+function recurrentMerge(input, value, output) {
+    output = reduce(value, (output, subValue, subKey) => {
+        if (subValue === undefined) {
+            return output;
+        }
+
+        const subInput = input[subKey];
+        let subOutput = output ? output[subKey] : undefined;
+        subOutput = valueMerge(subInput, subValue, subOutput);
+
+        if (subInput !== subOutput) {
+            if (!output) output = clone(input);
+            output[subKey] = subOutput;
+        }
+
+        return output;
+    }, output) || input;
+
+    return output;
+}
+
+export {merge};
