938bb7511f929e18af52bb7247f1c24351d00524
Grzegorz Wasiak <g.wasiak@gmail.com>
2017-03-19 21:38:44 +0100

    import/export bug fixed

--- src/immutably/immutably.js
+++ src/immutably/immutably.js
@@ -1,6 +1,6 @@
-import apply from './apply';
-import set from './set';
-import merge from './merge';
+import {apply} from './apply';
+import {set} from './set';
+import {merge} from './merge';
 
 const immutably = {apply, set, merge};
 
